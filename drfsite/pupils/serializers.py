eda34afbdbdae7208e9986a888ef5acf3471d810
rorame <roman.menyalschikov@yandex.ru>
2022-12-27 19:18:41 +0300

    introduction to serialization

--- drfsite/pupils/serializers.py
+++ drfsite/pupils/serializers.py
@@ -1,9 +1,43 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from .models import Pupils
 
+# Введение в сериализацию. Класс Serializer
+# class PupilModel :
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
-class PupilsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pupils
-        fields = ('title', 'cat_id')
+
+class PupilsSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=250)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = PupilModel('Varvara', 'Moscow')
+#     model_sr = PupilsSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json, type(json), sep='\n')
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Varvara","content":"Moscow"}')
+#     data = JSONParser().parse(stream)
+#     serializer = PupilsSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
+
+# class PupilsSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Pupils
+#         fields = ('title', 'cat_id')
--- drfsite/pupils/views.py
+++ drfsite/pupils/views.py
@@ -1,10 +1,36 @@
+from django.forms import model_to_dict
 from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Pupils
 from .serializers import PupilsSerializer
 
+# Базовый класс APIView для представлений
+class PupilsAPIView(APIView):
+    def get(self, request):
+        # lst = Pupils.objects.all().values()
+        # return Response({'pupils': list(lst)})
 
-class PupilsAPIView(generics.ListAPIView):
-    queryset = Pupils.objects.all()
-    serializer_class = PupilsSerializer
+        # Введение в сериализацию. Класс Serializer
+        pup = Pupils.objects.all()
+        return Response({'pupils': PupilsSerializer(pup, many=True).data})
+
+    def post(self, request):
+        # Введение в сериализацию. Класс Serializer
+        serializers = PupilsSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True )
+
+        post_new = Pupils.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'puplis': PupilsSerializer(post_new).data})
+        # return Response({'pupils': model_to_dict(post_new)})
+
+
+# class PupilsAPIView(generics.ListAPIView):
+#     queryset = Pupils.objects.all()
+#     serializer_class = PupilsSerializer
